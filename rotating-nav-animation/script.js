8bf9c35ad22e21fff029aa923813cebe62c0bcb9
DMO97 <davidmercadoprog@gmail.com>
2023-04-19 17:40:18 -0600

    Se agregan los estilos falta contenido bien definido

--- rotating-nav-animation/script.js
+++ rotating-nav-animation/script.js
@@ -4,11 +4,12 @@ const open = document.getElementById('open')
 const close = document.getElementById('close')
 
 /*Selector de todos los elementos catalogados como container */
-const container = documento.querySelectorAll('.container')
+const container = document.querySelector('.container')
 
 /*Escuchador de eventos a partir del click, lo segundo se ejecuta una función que agrega 
 la clase "show-nav" al elemento container. 
 Este elemento container es el contenedor del menú de navegación.*/
-open.addEventListerner('click', () => container.classList.add('show-nav'))
+open.addEventListener('click', () => container.classList.add('show-nav'))
 
-close.addEventListerner('click', () => container.classList.add('show-nav'))
+close.addEventListener('click', () => container.classList.remove('show-nav'))
+
